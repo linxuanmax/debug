56bf0f27a91b878e97b6e17a171d7ac442a68a8f
linxuan <yzlpython@163.com>
2018-05-25 09:59:49 +0800

    提交一个新文件

---
+++ input.py
@@ -0,0 +1,16 @@
+class Test(object):
+    k = []
+
+    def func(self, name):
+        self.k.append(name)
+        print(self.k)
+
+
+a = Test()
+a.func('jack')
+
+b = Test()
+b.func('jack')
+
+c = Test()
+c.func('jack')
